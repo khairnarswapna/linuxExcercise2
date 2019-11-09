a1d3559bdab579cc716cff1f2944332f76c3f086
khairnarswapna <khairnarswapna99@gmail.com>
2019-11-09 15:57:02 +0530

    shell scripting

---
+++ linuxshellPGm/whileloop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+isfulltime=1;
+isparttime=2;
+counter=1;
+hours=50;
+while [ $hours -gt $counter ]
+  do
+ empcheck=$(( RANDOM % 3 ))
+  if [ $empcheck -eq $isfulltime ]
+   then
+   counter=$(( $counter + 8 ))
+   elif [ $empcheck -eq $isparttime ]
+   then
+   counter=$(( $counter + 4 ))		
+   else
+   counter=$(( $counter + 0))
+   fi
+   salary=$(($counter*200))
+done
