1f2ad7ab2ee90255890836b86d78748d71c61d1d
mahendra kundare <mr.kundare@gmail.com>
2019-11-12 09:09:04 +0530

    updated newly program

--- linuxshellPGm/Demodictionary1.sh
+++ linuxshellPGm/Demodictionary1.sh
@@ -6,7 +6,8 @@ sounds[cow]="moo"
 sounds[birds]="tweet"
 sounds[wolf]="howl"
 echo "dog sound" ${sounds[dog]} #dog's sound
-echo "all Animal sound " ${sound[@]} #all values
+echo "all Animal sound " ${sounds[@]} #all values
 echo "Animal " ${!sounds[@]}   #All keys
 echo "Number of animals "${#sounds[@]} #numbers of elements
 unset sounds[dog] #delete dog
+echo "all animal sound " ${sounds[@]}
